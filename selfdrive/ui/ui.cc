582ae0ec99cab7543dd35457f809718a5c07ef1c
Martin Lillepuu <martin@mlp.ee>
2019-11-11 13:15:32 +0200

    fix reading gpsLocationExternal

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -1358,12 +1358,12 @@ static void eng_ui_draw_UI(UIState *s)
   {
     const UIScene *scene = &s->scene;
     const int bb_dml_w = 180;
-    const int bb_dml_x = (scene->ui_viz_rx + (bdr_is * 2));
-    const int bb_dml_y = (box_y + (bdr_is * 1.5)) + 220;
+    const int bb_dml_x = (scene->ui_viz_rx + (bdr_s * 2));
+    const int bb_dml_y = (box_y + (bdr_s * 1.5)) + 220;
 
     const int bb_dmr_w = 180;
-    const int bb_dmr_x = scene->ui_viz_rx + scene->ui_viz_rw - bb_dmr_w - (bdr_is * 2);
-    const int bb_dmr_y = (box_y + (bdr_is * 1.5)) + 220;
+    const int bb_dmr_x = scene->ui_viz_rx + scene->ui_viz_rw - bb_dmr_w - (bdr_s * 2);
+    const int bb_dmr_y = (box_y + (bdr_s * 1.5)) + 220;
 
     eng_ui_draw_measures_right(s, bb_dml_x, bb_dml_y, bb_dml_w);
     eng_ui_draw_measures_left(s, bb_dmr_x, bb_dmr_y, bb_dmr_w);
@@ -2147,8 +2147,8 @@ void handle_message(UIState *s, Message * msg) {
     s->scene.map_valid = datad.mapValid;
   // ENG UI START
   } else if (eventd.which == cereal_Event_gpsLocationExternal) {
-    struct cereal_gpsLocationExternal datad;
-    cereal_read_gpsLocationExternal(&datad, eventd.gpsLocationExternal);
+    struct cereal_GpsLocationData datad;
+    cereal_read_GpsLocationData(&datad, eventd.gpsLocationExternal);
     s->scene.gpsAccuracy = datad.accuracy;
 
     // set default values for display
