a4093e5ce9b89a2411e8d7f26e7712f630c329ff
Martin Lillepuu <martin@mlp.ee>
2019-11-11 13:28:29 +0200

    enable visual radar

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -39,7 +39,8 @@ class CarInterface(CarInterfaceBase):
     ret = car.CarParams.new_message()
 
     ret.carName = "subaru"
-    ret.radarOffCan = True
+    # enable visual radar
+    ret.radarOffCan = False
     ret.carFingerprint = candidate
     ret.carVin = vin
     ret.isPandaBlack = has_relay
