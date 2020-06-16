1735c434da4c8b5bb5de2954fd90f9008685a8f2
Martin Lillepuu <martin@mlp.ee>
2020-06-16 14:20:22 +0300

    Add replay tests for Subaru Outback

--- selfdrive/test/test_car_models.py
+++ selfdrive/test/test_car_models.py
@@ -352,6 +352,10 @@ routes = {
     'carFingerprint': VOLKSWAGEN.GOLF,
     'enableCamera': True,
   },
+  "5ab784f361e19b78|2020-06-08--16-30-41": {
+    'carFingerprint': SUBARU.OUTBACK,
+    'enableCamera': True,
+  },
   "791340bc01ed993d|2019-03-10--16-28-08": {
     'carFingerprint': SUBARU.IMPREZA,
     'enableCamera': True,
@@ -415,7 +419,6 @@ non_tested_cars = [
   TOYOTA.CHRH,
   TOYOTA.HIGHLANDERH,
   TOYOTA.HIGHLANDERH_TSS2,
-  SUBARU.OUTBACK,
   SUBARU.LEGACY,
   SUBARU.FORESTER,
 ]
